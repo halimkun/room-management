f8f16669c371e4bf62a08be86b453b16fd3f14bc
halimkun <ffaisalhalim@gmail.com>
2022-03-14 05:48:20 +0700

    add update and delete

--- app/Controllers/Kegiatan.php
+++ app/Controllers/Kegiatan.php
@@ -10,7 +10,8 @@ class Kegiatan extends BaseController
 
     protected $km;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->km = new KegiatanModel();
     }
 
@@ -24,19 +25,51 @@ class Kegiatan extends BaseController
         $data = [
             'agenda' => $this->request->getVar('agenda'),
             'ruang' => $this->request->getVar('ruang'),
-            'tanggal' => $this->request->getVar('tgl'),
+            'tanggal' => str_replace("T", " ", $this->request->getVar('tgl')),
             'pj' => $this->request->getVar('pj'),
             'keterangan' => $this->request->getVar('keterangan'),
             'status' => $this->request->getVar('status')
         ];
-
-        dd($data);
 
         if ($this->km->save($data)) {
             session()->setFlashdata('success', 'Data berhasil ditambahkan!');
             return redirect()->to(base_url('/admin/kegiatan'));
         } else {
             session()->setFlashdata('error', 'Data gagal ditambahkan!');
+            return redirect()->to(base_url('/admin/kegiatan'));
+        }
+    }
+
+    function update () {
+        $data = [
+            'id' => $this->request->getVar('kode'),
+            'agenda' => $this->request->getVar('agenda'),
+            'ruang' => $this->request->getVar('ruang'),
+            'tanggal' => str_replace("T", " ", $this->request->getVar('tgl')),
+            'pj' => $this->request->getVar('pj'),
+            'keterangan' => $this->request->getVar('keterangan'),
+            'status' => $this->request->getVar('status')
+        ];
+
+        if ($this->km->save($data)) {
+            session()->setFlashdata('success', 'Data berhasil diubah!');
+            return redirect()->to(base_url('/admin/kegiatan'));
+        } else {
+            session()->setFlashdata('error', 'Data gagal diubah!');
+            return redirect()->to(base_url('/admin/kegiatan'));
+        }
+    }
+
+    function delete($id = null) {
+        if($id !== null){
+            if ($this->km->delete($id)) {
+                session()->setFlashdata('success', 'Data berhasil dihapus!');
+                return redirect()->to(base_url('/admin/kegiatan'));
+            } else {
+                session()->setFlashdata('error', 'Data gagal dihapus!');
+                return redirect()->to(base_url('/admin/kegiatan'));
+            }
+        } else {
             return redirect()->to(base_url('/admin/kegiatan'));
         }
     }
--- app/Views/admin/kegiatan.php
+++ app/Views/admin/kegiatan.php
@@ -19,8 +19,8 @@
         <div class="col-12">
             <div class="card">
                 <div class="card-body p-3">
-                    
-                <!-- get flash data -->
+
+                    <!-- get flash data -->
                     <?php if (session()->getFlashdata('success')) : ?>
                         <div class="alert alert-success alert-dismissible show fade">
                             <div class="alert-body">
@@ -61,7 +61,7 @@
                                 <?php $i = 1; ?>
                                 <?php foreach ($kegiatan as $k) : ?>
                                     <tr>
-                                        <td><?= $i++; ?></td>
+                                        <td><?= $i++ ?></td>
                                         <td><?= $k->agenda; ?></td>
                                         <td><?= $k->ruang; ?></td>
                                         <td><?= $k->tanggal; ?></td>
@@ -81,11 +81,10 @@
                                             ?>
                                         </td>
                                         <td class="text-center">
-                                            <a href="<?= base_url('admin/kegiatan/edit/' . $k->id); ?>" class="btn btn-sm btn-primary"><i class="far fa-edit"></i></a>
-                                            <a href="<?= base_url('admin/kegiatan/delete/' . $k->id); ?>" class="btn btn-sm btn-danger"><i class="far fa-trash"></i></a>
+                                            <button class="btn btn-sm btn-primary" id="btnmodaledit-<?= $k->id ?>" data-toggle="modal" data-target="#modal_edit_data_kegiatan_<?= str_replace(" ", "_", $k->agenda) . $k->id ?>"><i class="far fa-edit"></i></button>
+                                            <button class="btn btn-sm btn-danger" data-confirm="Woops...|Apakah anda yakin akan menghapus data <b><?= $k->agenda ?></b>" data-confirm-yes="window.location = '/kegiatan/delete/<?= $k->id ?>';"><i class="far fa-trash"></i></button>
                                         </td>
                                     </tr>
-                                    <?php $i++; ?>
                                 <?php endforeach; ?>
                             </tbody>
                         </table>
@@ -104,7 +103,8 @@
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
-                <form action="/kegiatan/store" method="post">
+                <!-- form input data -->
+                <form action="/kegiatan/store" id="formKegiatan" method="post">
                     <div class="modal-body">
                         <?= csrf_field() ?>
                         <div class="row">
@@ -124,7 +124,7 @@
                                 </div>
                                 <div class="form-group">
                                     <label for="tgl">Tanggal</label>
-                                    <input type="date" class="form-control" id="tgl" name="tgl" placeholder="Tanggal" required>
+                                    <input type="datetime-local" class="form-control" id="tgl" name="tgl" placeholder="Tanggal" required>
                                 </div>
                                 <div class="form-group">
                                     <label for="pj">Penanggung Jawab</label>
@@ -170,7 +170,147 @@
     </div>
 </div>
 
-<script>
+<!-- Modal update kegiatan -->
+<?php foreach ($kegiatan as $k) : ?>
+    <div class="modal fade" tabindex="1" id="modal_edit_data_kegiatan_<?= str_replace(" ", "_", $k->agenda) . $k->id ?>" aria-labelledby="modal_edit_data_kegiatan_<?= str_replace(" ", "_", $k->agenda) ?>" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4>Edit data Kegiatan</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form action="/kegiatan/update" id="formUpdateKegiatan" method="post">
+                        <div class="modal-body">
+                            <?= csrf_field() ?> <input type="hidden" name="kode" value="<?= $k->id ?>">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="agenda">Agenda</label>
+                                        <input type="text" class="form-control" value="<?= $k->agenda ?>" id="agenda" name="agenda" placeholder="Agenda" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="ruang">Ruang</label>
+                                        <select name="ruang" id="ruang" class="custom-select">
+                                            <option value="">Pilih Ruang</option>
+                                            <?php foreach ($ruang as $r) : ?>
+                                                <option value="<?= $r->ruang; ?>" <?= $k->ruang == $r->ruang ? 'selected' : '' ?>><?= $r->ruang; ?></option>
+                                            <?php endforeach; ?>
+                                        </select>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="tgl">Tanggal</label>
+                                        <input type="datetime-local" class="form-control" value="<?= str_replace(" ", "T", $k->tanggal) ?>" id="tgl" name="tgl" placeholder="Tanggal" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="pj">Penanggung Jawab</label>
+                                        <select name="pj" id="pj" class="custom-select">
+                                            <option value="">Pilih Penanggung Jawab</option>
+                                            <?php foreach ($pj as $p) : ?>
+                                                <option value="<?= $p->nama; ?>" <?= $k->pj == $p->nama ? 'selected' : '' ?>><?= $p->nama; ?></option>
+                                            <?php endforeach; ?>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="keterangan">Keterangan</label>
+                                        <textarea name="keterangan" id="keterangan" class="form-control" placeholder="Keterangan" style="height: 137px !important"><?= $k->keterangan ?></textarea>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="status">Status</label>
+                                        <select name="status" id="status" class="custom-select">
+                                            <option <?= $k->status == "" ? 'selected' : '' ?> value="">Pilih Status</option>
+                                            <option <?= $k->status == "akan datang" ? 'selected' : '' ?> value="akan datang">Akan Datang</option>
+                                            <option <?= $k->status == "berlangsung" ? 'selected' : '' ?> value="berlangsung">Berlangsung</option>
+                                            <option <?= $k->status == "selesai" ? 'selected' : '' ?> value="selesai">Selesai</option>
+                                            <option <?= $k->status == "batal" ? 'selected' : '' ?> value="batal">Batal</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <div class="form-group text-right">
+                                <button type="reset" class="btn btn-secondary text-dark">
+                                    <span aria-hidden="true">&times; Cler Form</span>
+                                </button>
+                            </div>
+                            <div class="form-group text-right">
+                                <button type="submit" class="btn btn-primary"><i class="fa fa-save mr-2"></i> SIMPAN</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php endforeach; ?>
 
+<script>
+    $(document).ready(function() {
+        jQuery.validator.setDefaults({
+            errorClass: "is-invalid error",
+            validClass: "is-valid",
+        });
+
+        $.validator.addMethod("alphanumspacedash", function(value, element) {
+            return this.optional(element) || /^[a-z0-9\-\s]+$/i.test(value);
+        }, "Username must contain only letters, numbers, or dashes.");
+
+        $("#formKegiatan").validate({
+            rules: {
+                agenda: {
+                    required: true,
+                    alphanumspacedash: true,
+                    minlength: 10,
+                },
+                ruang: {
+                    required: true
+                },
+                tgl: {
+                    required: true
+                },
+                pj: {
+                    required: true
+                },
+                keterangan: {
+                    alphanumspacedash: true
+                },
+                status: {
+                    required: true
+                }
+            },
+        });
+
+        $("form#formUpdateKegiatan").each(function() {
+            $(this).validate({
+                rules: {
+                    agenda: {
+                        required: true,
+                        alphanumspacedash: true,
+                        minlength: 10,
+                    },
+                    ruang: {
+                        required: true
+                    },
+                    tgl: {
+                        required: true
+                    },
+                    pj: {
+                        required: true
+                    },
+                    keterangan: {
+                        alphanumspacedash: true
+                    },
+                    status: {
+                        required: true
+                    }
+                },
+            });
+        });
+    });
 </script>
 <?= $this->endSection(); ?>
